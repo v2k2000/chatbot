9fe86a8528956af6192c5c52f0a7e3c88a6b8e2a
shiunlee <shiun147@gmail.com>
2024-07-23 11:13:03 +0900

    bot

---
+++ 00_make_url.py
@@ -0,0 +1,22 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+
+URL = f'https://api.telegram.org/bot{TOKEN}'
+
+method = 'getUpdates'
+ 
+res = requests.get(f'{URL}/{method}')
+
+res_dict = res.json()
+
+user_input = res_dict['result'][-1]['message']['text']
+user_id = res_dict['result'][-1]['message']['from']['id']
+
+print(user_id, user_input)
+
+print(f'{URL}/sendMessage?chat_id={user_id}&text={user_input}')
